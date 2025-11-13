604a146bd618b2cf2e3545bdd863fe12f66d32eb
SergBul85 <donetsk55555@gmail.com>
2025-11-13 20:11:23 +0200

    Login fxml

--- src/main/java/com/example/bunchbank/App.java
+++ src/main/java/com/example/bunchbank/App.java
@@ -1,7 +1,20 @@
 package com.example.bunchbank;
 
-public class App {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+
+        stage.show();
+
 
     }
 }
