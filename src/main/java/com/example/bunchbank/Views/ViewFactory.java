96a28c2ed95ffaff2cf00ff40d8b448bc7cd116a
SergBul85 <donetsk55555@gmail.com>
2025-11-16 11:50:26 +0200

    ViewFactory

--- src/main/java/com/example/bunchbank/Views/ViewFactory.java
+++ src/main/java/com/example/bunchbank/Views/ViewFactory.java
@@ -1,4 +1,56 @@
 package com.example.bunchbank.Views;
 
+import com.example.bunchbank.Controllers.Client.ClientController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ViewFactory {
+
+    //Client Views
+    private static AnchorPane dashboardView;
+
+    public ViewFactory() {
+
+    }
+
+    public AnchorPane getDashboardView() {
+        if (dashboardView == null) {
+            try {
+                dashboardView = new FXMLLoader(getClass().getResource("/Fxml/Client/Dashboard.fxml")).load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return dashboardView;
+    }
+
+    public void showLoginWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Client/Login.fxml"));
+        createStage(loader);
+    }
+
+    public void showClientWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Client/Client.fxml"));
+        ClientController controller = new ClientController();
+        loader.setController(controller);
+        createStage(loader);
+    }
+
+    private static void createStage(FXMLLoader loader) {
+        Scene scene = null;
+        try {
+            scene = new Scene(loader.load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Bunch Bank");
+        stage.show();
+    }
+
 }
