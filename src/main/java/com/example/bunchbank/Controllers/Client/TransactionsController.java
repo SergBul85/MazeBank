8f7fda60ee65fbc35b330e6d2eb08c7db68c73d2
SergBul85 <donetsk55555@gmail.com>
2025-11-19 10:53:13 +0200

    87 Change Center Views

--- src/main/java/com/example/bunchbank/Controllers/Client/TransactionsController.java
+++ src/main/java/com/example/bunchbank/Controllers/Client/TransactionsController.java
@@ -7,20 +7,13 @@ import javafx.scene.control.ListView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-<<<<<<< HEAD
-public class TransactionsController implements Initializable {
 
+public class TransactionsController implements Initializable {
     @FXML
     public ListView transactions_listview;
-=======
-public class TransactionsController  implements Initializable {
-
-    @FXML
-    private ListView transactions_listview;
->>>>>>> 3c6d6159fa0cb1278173f6ae02682a8835052a8d
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 }
+
