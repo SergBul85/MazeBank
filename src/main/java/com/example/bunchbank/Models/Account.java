e3b28ab4809c3744d0aba2752fb8645c9caa16fd
SergBul85 <donetsk55555@gmail.com>
2025-11-23 22:08:49 +0200

    100 Account model

--- src/main/java/com/example/bunchbank/Models/Account.java
+++ src/main/java/com/example/bunchbank/Models/Account.java
@@ -1,5 +1,29 @@
 package com.example.bunchbank.Models;
 
+import javafx.beans.property.*;
+
 public class Account {
+
+    private final StringProperty owner;
+    private final StringProperty accountNumber;
+    private final DoubleProperty balance;
+
+    public Account(String owner, String accountNumber, double balance) {
+        this.owner = new SimpleStringProperty(this, "Owner", owner);
+        this.accountNumber = new SimpleStringProperty(this, "Account Number", accountNumber);
+        this.balance = new SimpleDoubleProperty(this, "Balance", balance);
+    }
+
+    public StringProperty ownerProperty() {
+        return owner;
+    }
+
+    public StringProperty accountNumberProperty() {
+        return accountNumber;
+    }
+
+    public DoubleProperty balanceProperty() {
+        return balance;
+    }
 
 }
