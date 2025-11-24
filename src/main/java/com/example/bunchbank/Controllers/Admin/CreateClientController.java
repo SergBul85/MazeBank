12fd9a135c29a50262e7454375fe79287112fc87
SergBul85 <donetsk55555@gmail.com>
2025-11-24 18:52:30 +0200

    106 Creating Clients in Admin

--- src/main/java/com/example/bunchbank/Controllers/Admin/CreateClientController.java
+++ src/main/java/com/example/bunchbank/Controllers/Admin/CreateClientController.java
@@ -1,5 +1,6 @@
 package com.example.bunchbank.Controllers.Admin;
 
+import com.example.bunchbank.Models.Model;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -7,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class CreateClientController implements Initializable {
@@ -22,8 +24,21 @@ public class CreateClientController implements Initializable {
     public Button create_client_btn;
     public Label error_lbl;
 
+    public String payeeAddress;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 
+    private void craeteCheckingAccount() {
+        double balance = Double.parseDouble(ch_amount_fld.getText());
+        // Generate Account Number
+        String firstSection = "3201";
+        String lastSection = Integer.toString(new Random().nextInt(9999) + 1000);
+        String accountNumber = firstSection + " " + lastSection;
+        //Create the Checking Account
+        Model.getInstance().getDataBaseDriver().createCheckingAccount(
+                payeeAddress, accountNumber, 10, balance
+        );
     }
 }
--- src/main/java/com/example/bunchbank/Models/DataBaseDriver.java
+++ src/main/java/com/example/bunchbank/Models/DataBaseDriver.java
@@ -1,6 +1,8 @@
 package com.example.bunchbank.Models;
 
 import java.sql.*;
+import java.text.MessageFormat;
+import java.time.LocalDate;
 
 public class DataBaseDriver {
 
@@ -31,7 +33,6 @@ public class DataBaseDriver {
         return resultSet;
     }
 
-
     //
 //    Admin Section
 //
@@ -45,6 +46,43 @@ public class DataBaseDriver {
             e.printStackTrace();
         }
         return resultSet;
+    }
+
+    public void createClient(String fName, String lName, String pAddress, String password, LocalDate date) {
+        Statement statement;
+        try {
+            statement = this.conn.createStatement();
+            statement.executeUpdate(
+                    MessageFormat.format("INSERT INTO Clients (FirstName, LastName, PayeeAddress, Password, Date) " +
+                            "VALUES (''{0}'', ''{1}'', ''{2}'', ''{3}'', ''{4}'')", fName, lName, pAddress, password, date)
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void createCheckingAccount(String owner, String number, double tLimit, double balance) {
+        Statement statement;
+        try {
+            statement = this.conn.createStatement();
+            statement.executeUpdate(MessageFormat.format("INSERT INTO CheckingAccounts (Owner, AccountNumber, TransactionLimit, Balance) " +
+                    "VALUES (''{0}'', ''{1}'', ''{2}'', ''{3}'')", owner, number, tLimit, balance));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createSavingsAccount(String owner, String number, double withdrawalLimit, double balance) {
+        Statement statement;
+        try {
+            statement = this.conn.createStatement();
+            statement.executeUpdate(MessageFormat.format("INSERT INTO CheckingAccounts " +
+                    "(Owner, AccountNumber, WithdrawalLimit, Balance) " +
+                    "VALUES (''{0}'', ''{1}'', ''{2}'', ''{3}'')", owner, number, withdrawalLimit, balance));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     //
