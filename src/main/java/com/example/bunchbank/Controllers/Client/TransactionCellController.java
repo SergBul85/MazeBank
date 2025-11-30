f556aa64d4418a85590d6d9e8c3af6379058603d
SergBul85 <donetsk55555@gmail.com>
2025-11-30 14:48:28 +0200

    116 Client Transaction Message

--- src/main/java/com/example/bunchbank/Controllers/Client/TransactionCellController.java
+++ src/main/java/com/example/bunchbank/Controllers/Client/TransactionCellController.java
@@ -4,6 +4,7 @@ import com.example.bunchbank.Models.Model;
 import com.example.bunchbank.Models.Transaction;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 
@@ -17,6 +18,7 @@ public class TransactionCellController implements Initializable {
     public Label trans_date_lbl;
     public Label sender_lbl;
     public Label receiver_lbl;
+    public Button message_btn;
     public Label amount_lbl;
 
     private final Transaction transaction;
@@ -31,6 +33,8 @@ public class TransactionCellController implements Initializable {
         sender_lbl.textProperty().bind(transaction.senderProperty());
         receiver_lbl.textProperty().bind(transaction.receiverProperty());
         amount_lbl.textProperty().bind(transaction.amountProperty().asString());
+        message_btn.setOnAction(event ->
+                Model.getInstance().getViewFactory().showMessageWindow(transaction.senderProperty().get(), transaction.messageProperty().get()));
         transactionIcon();
     }
 
